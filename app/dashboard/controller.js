2446e0ab8a178c52b83360723a3817dcd9dc5db3
Alvin <alvinmartindwrk@gmail.com>
2022-04-23 21:09:40 +0700

    create dashboard controller & page

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,10 +1,22 @@
+const Transaction = require('../transaction/model');
+const Voucher = require('../voucher/model');
+const Player = require('../player/model');
+const Category = require('../category/model');
+
 module.exports = {
   index: async (req, res) => {
     try {
       // console.log(req.session.user);
-      res.render('index', {
+      const transaction = await Transaction.countDocuments();
+      const voucher = await Voucher.countDocuments();
+      const player = await Player.countDocuments();
+      const category = await Category.countDocuments();
+      res.render('admin/dashboard/view_dashboard', {
         name: req.session.user.name,
         title: 'Dashboard',
+        count: {
+          transaction, voucher, player, category,
+        },
       });
     } catch (error) {
       console.log(error);
--- app/player/model.js
+++ app/player/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const playerSchema = mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email address is required'],
@@ -10,6 +10,12 @@ const userSchema = mongoose.Schema({
     required: [true, 'Name is required'],
     minlength: [3, 'Name must be at least 3 characters'],
     maxlength: [225, 'Name must be less than or equal to 225 characters'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    minlength: [3, 'Username must be at least 3 characters'],
+    maxlength: [225, 'Username must be less than or equal to 225 characters'],
   },
   password: {
     type: String,
@@ -26,13 +32,23 @@ const userSchema = mongoose.Schema({
   role: {
     type: String,
     enum: ['admin', 'user'],
-    default: 'admin',
+    default: 'user',
   },
   status: {
     type: String,
     enum: ['Y', 'N'],
     default: 'Y',
   },
+  avatar: {
+    type: String,
+  },
+  fileName: {
+    type: String,
+  },
+  favorite: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Player', playerSchema);
