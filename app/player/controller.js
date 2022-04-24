246c7c5715f4f76be0fbe8f920e26318a62f7259
Alvin <alvinmartindwrk@gmail.com>
2022-04-24 21:49:11 +0700

    get all category api

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require('./model');
 const Voucher = require('../voucher/model');
+const Category = require('../category/model');
 
 module.exports = {
   index: async (req, res) => {
@@ -48,6 +49,16 @@ module.exports = {
       });
     } catch (error) {
       return res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      res.status(200).json({ data: category });
+    } catch (error) {
+      res.status(500).json({
         message: error.message || 'Internal Server Error',
       });
     }
--- app/player/router.js
+++ app/player/router.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { landingPage, detailPage } = require('./controller');
+const { landingPage, detailPage, category } = require('./controller');
 
 /* starts with /api/v1/players */
 
 router.get('/landing-page', landingPage);
 router.get('/:id/detail', detailPage);
+router.get('/category', category);
 
 module.exports = router;
