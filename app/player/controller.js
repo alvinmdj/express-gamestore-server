e1390da295cf973b71535be77f9e4f39b1d2c14e
Alvin <alvinmartindwrk@gmail.com>
2022-04-24 20:57:41 +0700

    create detail page api

--- app/player/controller.js
+++ app/player/controller.js
@@ -29,4 +29,27 @@ module.exports = {
       });
     }
   },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findById(id)
+        .populate('category')
+        .populate('nominals')
+        .populate('user', '_id name phoneNumber');
+
+      if (!voucher) {
+        return res.status(404).json({
+          message: 'Game voucher not found',
+        });
+      }
+
+      return res.status(200).json({
+        data: voucher,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const { landingPage } = require('./controller');
+const { landingPage, detailPage } = require('./controller');
 
 /* starts with /api/v1/players */
 
 router.get('/landing-page', landingPage);
+router.get('/:id/detail', detailPage);
 
 module.exports = router;
